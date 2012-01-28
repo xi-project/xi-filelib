af5d6c1b15852a9af64462aa29c62877a62520ee
Mikko Hirvonen <mikko.petteri.hirvonen@gmail.com>
2012-01-28 20:22:52 +0200

    Cleaned up.

--- tests/Xi/Tests/Filelib/Backend/ZendDbBackendTest.php
+++ tests/Xi/Tests/Filelib/Backend/ZendDbBackendTest.php
@@ -3,34 +3,24 @@
 namespace Xi\Tests\Filelib\Backend;
 
 use Xi\Filelib\Backend\ZendDbBackend,
-    \Zend_Db,
-    Xi\Filelib\Folder\FolderItem,
-    Xi\Filelib\File\FileItem,
-    \DateTime
-    ;
+    Zend_Db;
 
 /**
- * Description of ZendDbTest
- *
  * @author pekkis
  * @group  zenddb
  */
 class ZendDbBackendTest extends RelationalDbTestCase
 {
     /**
-     *
      * @var ZendDbBackend
      */
     protected $backend;
-    
-    
-    protected $conn;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->conn = Zend_Db::factory('pdo_' . PDO_DRIVER, array(
+        $db = Zend_Db::factory('pdo_' . PDO_DRIVER, array(
             'host'     => PDO_HOST,
             'dbname'   => PDO_DBNAME,
             'username' => PDO_USERNAME,
@@ -38,23 +28,22 @@ class ZendDbBackendTest extends RelationalDbTestCase
         ));
 
         $this->backend = new ZendDbBackend();
-        $this->backend->setDb($this->conn);
-        
-        // $conn = $this->getConnection()->getConnection();
-        
-                
-        // $conn->exec('DELETE FROM xi_filelib_folder');
-       // $n->exec("DELETE FROM sqlite_sequence where name='xi_filelib_folder'");
-        
-                
+        $this->backend->setDb($db);
     }
-    
+
     /**
      * @test
      */
     public function zendDbGettersShouldReturnCorrectObjects()
     {
-        $this->assertInstanceOf('Xi\Filelib\Backend\ZendDb\FileTable', $this->backend->getFileTable());
-        $this->assertInstanceOf('Xi\Filelib\Backend\ZendDb\FolderTable', $this->backend->getFolderTable());
+        $this->assertInstanceOf(
+            'Xi\Filelib\Backend\ZendDb\FileTable',
+            $this->backend->getFileTable()
+        );
+
+        $this->assertInstanceOf(
+            'Xi\Filelib\Backend\ZendDb\FolderTable',
+            $this->backend->getFolderTable()
+        );
     }
 }
