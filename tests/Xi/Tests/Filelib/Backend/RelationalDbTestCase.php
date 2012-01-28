c4c680a26e063b3ff453ca1bcf3d310cfb1db018
Mikko Hirvonen <mikko.petteri.hirvonen@gmail.com>
2012-01-28 13:25:58 +0200

    Moved more tests to base class.

--- tests/Xi/Tests/Filelib/Backend/RelationalDbTestCase.php
+++ tests/Xi/Tests/Filelib/Backend/RelationalDbTestCase.php
@@ -162,4 +162,273 @@ abstract class RelationalDbTestCase extends DbTestCase
 
         $this->assertFalse($this->backend->deleteFolder($folder));
     }
+
+    /**
+     * @test
+     */
+    public function updateFolderShouldUpdateFolder()
+    {
+        $data = array(
+            'id'        => 3,
+            'parent_id' => 2,
+            'url'       => 'lussuttaja/tussin',
+            'name'      => 'tussin',
+        );
+
+        $this->assertEquals($data, $this->backend->findFolder(3));
+
+        $updateData = array(
+            'id'        => 3,
+            'parent_id' => 1,
+            'url'       => 'lussuttaja/lussander',
+            'name'      => 'lussander',
+        );
+
+        $folder = FolderItem::create($updateData);
+
+        $this->assertTrue($this->backend->updateFolder($folder));
+        $this->assertEquals($updateData, $this->backend->findFolder(3));
+    }
+
+    /**
+     * @test
+     */
+    public function updatesRootFolder()
+    {
+        $data = array(
+            'id'        => 3,
+            'parent_id' => null,
+            'url'       => 'foo/bar',
+            'name'      => 'xoo',
+        );
+
+        $folder = FolderItem::create($data);
+
+        $this->assertTrue($this->backend->updateFolder($folder));
+        $this->assertEquals($data, $this->backend->findFolder(3));
+    }
+
+    /**
+     * @test
+     */
+    public function updateFolderShouldNotUpdateNonExistingFolder()
+    {
+        $folder = FolderItem::create(array(
+            'id'        => 333,
+            'parent_id' => 1,
+            'url'       => 'lussuttaja/lussander',
+            'name'      => 'lussander',
+        ));
+
+        $this->assertFalse($this->backend->updateFolder($folder));
+    }
+
+    /**
+     * @test
+     * @expectedException Xi\Filelib\FilelibException
+     */
+    public function updateFolderShouldThrowExceptionWhenUpdatingErroneousFolder()
+    {
+        $folder = FolderItem::create(array(
+            'id'        => 'xoofiili',
+            'parent_id' => 'xoo',
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $this->assertFalse($this->backend->updateFolder($folder));
+    }
+
+    /**
+     * @test
+     */
+    public function findSubFoldersShouldReturnArrayOfSubFolders()
+    {
+        $folder = FolderItem::create(array(
+            'id'        => 1,
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findSubFolders($folder);
+
+        $this->assertInternalType('array', $ret);
+        $this->assertCount(1, $ret);
+
+        $folder = FolderItem::create(array(
+            'id'        => 2,
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findSubFolders($folder);
+
+        $this->assertInternalType('array', $ret);
+        $this->assertCount(3, $ret);
+
+        $folder = FolderItem::create(array(
+            'id'        => 4,
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findSubFolders($folder);
+
+        $this->assertInternalType('array', $ret);
+        $this->assertCount(0, $ret);
+    }
+
+    /**
+     * @test
+     * @expectedException Xi\Filelib\FilelibException
+     */
+    public function findSubFoldersShouldThrowExceptionForErroneousFolder()
+    {
+        $folder = FolderItem::create(array(
+            'id'        => 'xooxer',
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $this->backend->findSubFolders($folder);
+    }
+
+    /**
+     * @test
+     */
+    public function findFolderByUrlShouldReturnFolder()
+    {
+        $ret = $this->backend->findFolderByUrl('lussuttaja/tussin');
+
+        $this->assertInternalType('array', $ret);
+        $this->assertEquals(3, $ret['id']);
+    }
+
+    /**
+     * @test
+     */
+    public function findFolderByUrlShouldNotReturnNonExistingFolder()
+    {
+        $this->assertFalse(
+            $this->backend->findFolderByUrl('lussuttaja/tussinnnnn')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function findFilesInShouldReturnArrayOfFiles()
+    {
+        $folder = FolderItem::create(array(
+            'id'        => 1,
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findFilesIn($folder);
+
+        $this->assertInternalType('array', $ret);
+        $this->assertCount(1, $ret);
+
+        $folder = FolderItem::create(array(
+            'id'        => 4,
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findFilesIn($folder);
+
+        $this->assertInternalType('array', $ret);
+        $this->assertCount(2, $ret);
+
+        $folder = FolderItem::create(array(
+            'id'        => 5,
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findFilesIn($folder);
+
+        $this->assertInternalType('array', $ret);
+        $this->assertCount(0, $ret);
+    }
+
+    /**
+     * @test
+     * @expectedException Xi\Filelib\FilelibException
+     */
+    public function findFilesInShouldThrowExceptionWithErroneousFolder()
+    {
+        $folder = FolderItem::create(array(
+            'id'        => 'xoo',
+            'parent_id' => null,
+            'url'       => '',
+            'name'      => '',
+        ));
+
+        $ret = $this->backend->findFilesIn($folder);
+    }
+
+    /**
+     * @test
+     */
+    public function findFileShouldReturnFile()
+    {
+        $ret = $this->backend->findFile(1);
+
+        $this->assertInternalType('array', $ret);
+
+        $this->assertArrayHasKey('id', $ret);
+        $this->assertArrayHasKey('folder_id', $ret);
+        $this->assertArrayHasKey('mimetype', $ret);
+        $this->assertArrayHasKey('profile', $ret);
+        $this->assertArrayHasKey('size', $ret);
+        $this->assertArrayHasKey('name', $ret);
+        $this->assertArrayHasKey('link', $ret);
+        $this->assertArrayHasKey('date_uploaded', $ret);
+
+        $this->assertInstanceOf('DateTime', $ret['date_uploaded']);
+    }
+
+    /**
+     * @test
+     * @expectedException Xi\Filelib\FilelibException
+     */
+    public function findFileShouldThrowExceptionWithErroneousId()
+    {
+        $this->backend->findFile('xooxoeroe');
+    }
+
+    /**
+     * @test
+     */
+    public function findAllFilesShouldReturnAllFiles()
+    {
+        $rets = $this->backend->findAllFiles();
+
+        $this->assertInternalType('array', $rets);
+        $this->assertCount(5, $rets);
+
+        foreach ($rets as $ret) {
+            $this->assertInternalType('array', $ret);
+
+            $this->assertArrayHasKey('id', $ret);
+            $this->assertArrayHasKey('folder_id', $ret);
+            $this->assertArrayHasKey('mimetype', $ret);
+            $this->assertArrayHasKey('profile', $ret);
+            $this->assertArrayHasKey('size', $ret);
+            $this->assertArrayHasKey('name', $ret);
+            $this->assertArrayHasKey('link', $ret);
+            $this->assertArrayHasKey('date_uploaded', $ret);
+
+            $this->assertInstanceOf('DateTime', $ret['date_uploaded']);
+        }
+    }
 }
