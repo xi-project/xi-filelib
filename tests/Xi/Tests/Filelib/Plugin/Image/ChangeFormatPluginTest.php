d9a3ef3440a759d5d09c4f23a1a57e0ef4cc6329
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-02-12 21:06:18 +0200

    Added test skeletors.

---
+++ tests/Xi/Tests/Filelib/Plugin/Image/ChangeFormatPluginTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Xi\Tests\Filelib\Plugin\Image;
+
+use Imagick;
+use Xi\Filelib\Plugin\Image\ChangeFormatPlugin;
+
+class ChangeFormatPluginTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function classShouldExist()
+    {
+        $this->assertTrue(class_exists('Xi\Filelib\Plugin\Image\ChangeFormatPlugin'));
+    }
+    
+    
+    
+    
+}
