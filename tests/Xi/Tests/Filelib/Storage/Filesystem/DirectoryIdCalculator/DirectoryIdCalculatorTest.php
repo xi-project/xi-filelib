a113dba7d626ee5d5434cbd8533455753dbd9eb4
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-02-12 21:53:13 +0200

    Added test skeletors.

---
+++ tests/Xi/Tests/Filelib/Storage/Filesystem/DirectoryIdCalculator/DirectoryIdCalculatorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Xi\Tests\Filelib\Storage\Filesystem\DirectoryIdCalculator;
+
+use Xi\Tests\Filelib\TestCase;
+
+class DirectoryIdCalculatorTest extends TestCase
+{
+    
+    /**
+     * @test
+     */
+    public function interfaceShouldExist()
+    {
+        $this->assertTrue(interface_exists('Xi\Filelib\Storage\Filesystem\DirectoryIdCalculator\DirectoryIdCalculator'));
+    }
+    
+    
+}
