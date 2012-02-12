c8e971312e981ef011a65baa2118dd5d6f64eba9
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-02-12 19:52:45 +0200

    Tested image commands & fucktored code.

--- library/Xi/Filelib/Plugin/Image/Command/AbstractCommand.php
+++ library/Xi/Filelib/Plugin/Image/Command/AbstractCommand.php
@@ -2,7 +2,10 @@
 
 namespace Xi\Filelib\Plugin\Image\Command;
 
-use \Imagick;
+use Imagick;
+use ImagickException;
+use InvalidArgumentException;
+use Xi\Filelib\Configurator;
 
 /**
  * Abstract convenience class for versionplugin plugins
@@ -12,10 +15,26 @@ use \Imagick;
  */
 abstract class AbstractCommand implements Command
 {
-    
+
     public function __construct($options = array())
     {
-        \Xi\Filelib\Configurator::setConstructorOptions($this, $options);
+        Configurator::setConstructorOptions($this, $options);
     }
-    
+
+    /**
+     * Creates a new imagick resource from path
+     * 
+     * @param string $path Image path
+     * @return Imagick
+     * @throws InvalidArgumentException
+     */
+    public function createImagick($path)
+    {
+        try {
+            return new Imagick($path);
+        } catch (ImagickException $e) {
+            throw new InvalidArgumentException(sprintf("ImageMagick could not be created from path '%s'", $path), 500, $e);
+        }
+    }
+
 }
---
+++ tests/Xi/Tests/Filelib/Plugin/Image/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Xi\Tests\Filelib\Plugin\Image;
+
+use \Imagick;
+
+class TestCase extends \Xi\Tests\Filelib\TestCase
+{
+
+    public function setUp()
+    {
+        if (!class_exists('Imagick')) {
+            $this->markTestSkipped('ImageMagick extension not loaded');
+            return;
+        }
+    }
+    
+    
+    public function tearDown()
+    {
+        if (!class_exists('Imagick')) {
+            $this->markTestSkipped('ImageMagick extension not loaded');
+            return;
+        }
+    }
+    
+    
+    
+}
