69b4efdfa331af1f30108cbc434e61badb70fac3
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-02-24 21:51:19 +0200

    Added file and fileupload events.

---
+++ tests/Xi/Tests/Filelib/Event/FileEventTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Xi\Tests\Filelib\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use Xi\Filelib\Event\FileEvent;
+
+class FileEventTest extends \Xi\Tests\Filelib\TestCase
+{
+    
+    /**
+     * @test
+     */
+    public function classShouldExist()
+    {
+        $this->assertTrue(class_exists('Xi\Filelib\Event\FileEvent'));
+        $this->assertContains('Symfony\Component\EventDispatcher\Event', class_parents('Xi\Filelib\Event\FileEvent'));
+    }
+    
+    /**
+     * @test
+     */
+    public function eventShouldInitializeCorrectly()
+    {
+        $file = $this->getMockForAbstractClass('Xi\Filelib\File\File');
+        
+        $event = new FileEvent($file);
+        
+        $file2 = $event->getFile();
+        
+        $this->assertSame($file, $file2);
+        
+    }
+    
+    
+}
