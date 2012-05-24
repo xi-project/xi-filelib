f2c5e23b2f3642aaa816afceb9ead6065bb5d6bb
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-05-24 15:51:34 +0300

    Uuid to folders.

--- docs/schema-sqlite.sql
+++ docs/schema-sqlite.sql
@@ -4,6 +4,7 @@ CREATE UNIQUE INDEX UNIQ_E8606524D17F50A6 ON xi_filelib_file (uuid);
 CREATE INDEX IDX_E8606524162CB942 ON xi_filelib_file (folder_id);
 CREATE INDEX IDX_E860652489329D25 ON xi_filelib_file (resource_id);
 CREATE UNIQUE INDEX folderid_filename_unique ON xi_filelib_file (folder_id, filename);
-CREATE TABLE xi_filelib_folder (id INTEGER NOT NULL, parent_id INTEGER DEFAULT NULL, foldername VARCHAR(255) NOT NULL, folderurl CLOB NOT NULL, PRIMARY KEY("id"));
+CREATE TABLE xi_filelib_folder (id INTEGER NOT NULL, parent_id INTEGER DEFAULT NULL, foldername VARCHAR(255) NOT NULL, folderurl CLOB NOT NULL, uuid VARCHAR(36) NOT NULL, PRIMARY KEY("id"));
+CREATE UNIQUE INDEX UNIQ_A5EA9E8BD17F50A6 ON xi_filelib_folder (uuid);
 CREATE INDEX IDX_A5EA9E8B727ACA70 ON xi_filelib_folder (parent_id);
 CREATE TABLE xi_filelib_resource (id INTEGER NOT NULL, hash VARCHAR(255) NOT NULL, date_created DATETIME NOT NULL, PRIMARY KEY("id"));
