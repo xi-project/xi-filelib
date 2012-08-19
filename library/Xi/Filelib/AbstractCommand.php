0a0e7d5df776003b691094ec2117bf39e13cc9ac
Mikko Hirvonen <mikko.petteri.hirvonen@gmail.com>
2012-08-19 11:50:28 +0300

    CS fixes.

--- library/Xi/Filelib/AbstractCommand.php
+++ library/Xi/Filelib/AbstractCommand.php
@@ -1,27 +1,42 @@
 <?php
 
+/**
+ * This file is part of the Xi Filelib package.
+ *
+ * For copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Xi\Filelib;
 
 abstract class AbstractCommand implements Command
 {
-
+    /**
+     * @var string
+     */
     protected $uuid;
 
+    /**
+     * @param string $uuid
+     */
     public function __construct($uuid)
     {
         $this->uuid = $uuid;
     }
 
-
+    /**
+     * @return string
+     */
     public function getUuid()
     {
         return $this->uuid;
     }
 
-
+    /**
+     * @return string
+     */
     public function getEnqueueReturnValue()
     {
         return $this->getUuid();
     }
-
 }
