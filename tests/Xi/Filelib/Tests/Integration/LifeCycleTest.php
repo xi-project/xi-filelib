a803a8008ce2bdb61ad14766d949adeffc658e86
Mikko Forsström <puhemies@diktaattoriporssi.com>
2014-03-31 22:56:37 +0300

    Added first end-to-end integration tests.

---
+++ tests/Xi/Filelib/Tests/Integration/LifeCycleTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Xi\Filelib\Tests\Integration;
+
+
+class LifeCycleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function nothingIsFoundAfterDeleting()
+    {
+        $manateePath = ROOT_TESTS . '/data/self-lussing-manatee.jpg';
+
+        $folder = $this->filelib->getFolderRepository()->createByUrl('imaiseppa/mehevaa/soprano/ja/arto-tenhunen');
+
+        $this->assertInstanceOf('Xi\Filelib\Folder\Folder', $folder);
+
+        $file = $this->filelib->upload($manateePath, $folder);
+
+        $this->assertInstanceOf('Xi\Filelib\File\File', $file);
+
+        $allFiles = $this->filelib->getFileRepository()->findAll();
+        $this->assertCount(1, $allFiles);
+        $this->assertSame($file, $allFiles->current());
+
+        $this->filelib->getFileRepository()->delete($file);
+
+        $allFiles = $this->filelib->getFileRepository()->findAll();
+        $this->assertCount(0, $allFiles);
+
+        $this->assertStorageFileCount(3);
+        $this->assertPublisherFileCount(0);
+
+        $allResources = $this->filelib->getResourceRepository()->findAll();
+        $this->assertCount(1, $allResources);
+
+        $secondFile =  $this->filelib->upload($manateePath);
+        $this->assertSame($file->getResource(), $secondFile->getResource());
+
+        $this->publisher->publish($secondFile);
+        $this->assertStorageFileCount(3);
+        $this->assertPublisherFileCount(2);
+
+        $this->filelib->getFileRepository()->delete($secondFile);
+        $this->assertStorageFileCount(3);
+        $this->assertPublisherFileCount(0);
+
+        $this->filelib->getResourceRepository()->delete($allResources->current());
+        $this->assertStorageFileCount(0);
+
+        $allResources = $this->filelib->getResourceRepository()->findAll();
+        $this->assertCount(0, $allResources);
+    }
+}
