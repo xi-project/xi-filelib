cea665c8b7aba63b5a42452a171c0382387bee8f
Mikko Forsström <puhemies@diktaattoriporssi.com>
2014-04-03 10:30:27 +0300

    Added a pre-commented cache block.

--- docs/html/bootstrap.php
+++ docs/html/bootstrap.php
@@ -5,6 +5,8 @@ use Xi\Filelib\Backend\Adapter\JsonBackendAdapter;
 use Xi\Filelib\Storage\FilesystemStorage;
 use Xi\Filelib\Plugin\RandomizeNamePlugin;
 use Xi\Filelib\Storage\Filesystem\DirectoryIdCalculator\TimeDirectoryIdCalculator;
+use Xi\Filelib\Backend\Cache\Cache;
+use Xi\Filelib\Backend\Cache\Adapter\MemcachedCacheAdapter;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -12,6 +14,15 @@ $filelib = new FileLibrary(
     new FilesystemStorage(realpath(__DIR__ . '/data/private'), new TimeDirectoryIdCalculator()),
     new JsonBackendAdapter(__DIR__ . '/filelib-example.json')
 );
+
+/*
+// Uncomment me to enable caching
+$memcached = new \Memcached();
+$memcached->addServer('localhost', 11211);
+$filelib->createCacheFromAdapter(
+    new Cache(new MemcachedCacheAdapter($memcached))
+);
+*/
 
 // Randomizes the name of the uploaded file every time
 $filelib->addPlugin(new RandomizeNamePlugin());
