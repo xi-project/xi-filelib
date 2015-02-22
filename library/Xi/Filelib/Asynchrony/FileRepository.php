10ec46ac409f9cbe9fd7f157e57f134b8d416c58
Mikko Forsström <puhemies@diktaattoriporssi.com>
2015-02-22 11:37:09 +0200

    Refactorings continue.

--- library/Xi/Filelib/Asynchrony/FileRepository.php
+++ library/Xi/Filelib/Asynchrony/FileRepository.php
@@ -9,6 +9,8 @@
 
 namespace Xi\Filelib\Asynchrony;
 
+use Xi\Collections\Collection\ArrayCollection;
+use Xi\Filelib\Backend\Finder\FileFinder;
 use Xi\Filelib\File\File;
 use Xi\Filelib\File\FileRepository as BaseFileRepository;
 use Xi\Filelib\File\FileRepositoryInterface;
@@ -19,9 +21,6 @@ class FileRepository implements FileRepositoryInterface
 {
     const COMMAND_UPLOAD = 'xi_filelib.asynchrony.upload';
     const COMMAND_AFTERUPLOAD = 'xi_filelib.asynchrony.afterUpload';
-    const COMMAND_UPDATE = 'xi_filelib.asynchrony.update';
-    const COMMAND_DELETE = 'xi_filelib.asynchrony.delete';
-    const COMMAND_COPY = 'xi_filelib.asynchrony.copy';
 
     /**
      * @var BaseFileRepository
@@ -99,12 +98,6 @@ class FileRepository implements FileRepositoryInterface
         $this->strategies[$command] = $strategy;
     }
 
-    public function find($id)
-    {
-        return $this->innerRepository->find($id);
-    }
-
-
     public function upload($upload, Folder $folder = null, $profile = 'default')
     {
         return $this->execute(
@@ -129,13 +122,103 @@ class FileRepository implements FileRepositoryInterface
         );
     }
 
-    public function __call($method, $params)
+    /**
+     * Updates a file
+     *
+     * @param  File         $file
+     * @return FileRepository
+     */
+    public function update(File $file)
     {
-        $callback = [$this->innerRepository, $method];
-        $strategy = $this->asynchrony->getStrategy(ExecutionStrategies::STRATEGY_SYNC);
-        return $strategy->execute($callback, $params);
+        return $this->innerRepository->update($file);
     }
 
+    /**
+     * Finds file by id
+     *
+     * @param  mixed $id File id or array of file ids
+     * @return File
+     */
+    public function find($id)
+    {
+        return $this->innerRepository->find($id);
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function findMany($ids)
+    {
+        return $this->innerRepository->findMany($ids);
+    }
+
+    /**
+     * @param FileFinder $finder
+     * @return ArrayCollection
+     */
+    public function findBy(FileFinder $finder)
+    {
+        return $this->innerRepository->findBy($finder);
+    }
+
+    /**
+     * @param $uuid
+     * @return File
+     */
+    public function findByUuid($uuid)
+    {
+        return $this->innerRepository->findByUuid($uuid);
+    }
+
+    /**
+     * Finds file by filename in a folder
+     *
+     * @param Folder $folder
+     * @param $filename
+     * @return File
+     */
+    public function findByFilename(Folder $folder, $filename)
+    {
+        return $this->innerRepository->findByFilename($folder, $filename);
+    }
+
+    /**
+     * Finds and returns all files
+     *
+     * @return ArrayCollection
+     */
+    public function findAll()
+    {
+        return $this->innerRepository->findAll();
+    }
+
+    /**
+     * Deletes a file
+     *
+     * @param File $file
+     */
+    public function delete(File $file)
+    {
+        return $this->innerRepository->delete($file);
+    }
+
+    /**
+     * Copies a file to folder
+     *
+     * @param File   $file
+     * @param Folder $folder
+     */
+    public function copy(File $file, Folder $folder)
+    {
+        return $this->innerRepository->copy($file, $folder);
+    }
+
+    /**
+     * @param string $command
+     * @param array $callback
+     * @param array $params
+     * @return mixed
+     */
     private function execute($command, $callback, $params)
     {
         $strategy = $this->asynchrony->getStrategy(
@@ -144,5 +227,4 @@ class FileRepository implements FileRepositoryInterface
 
         return $strategy->execute($callback, $params);
     }
-
 }
--- library/Xi/Filelib/File/FileRepositoryInterface.php
+++ library/Xi/Filelib/File/FileRepositoryInterface.php
@@ -3,12 +3,19 @@
 namespace Xi\Filelib\File;
 
 use Xi\Filelib\Folder\Folder;
+use Xi\Collections\Collection\ArrayCollection;
+use Xi\Filelib\Backend\Finder\FileFinder;
+use Xi\Filelib\FilelibException;
 
 interface FileRepositoryInterface
 {
-    public function upload($upload, Folder $folder = null, $profile = 'default');
-
-    public function afterUpload(File $file);
+    /**
+     * Updates a file
+     *
+     * @param  File         $file
+     * @return FileRepository
+     */
+    public function update(File $file);
 
     /**
      * Finds file by id
@@ -18,4 +25,63 @@ interface FileRepositoryInterface
      */
     public function find($id);
 
+    /**
+     * @return ArrayCollection
+     */
+    public function findMany($ids);
+
+    /**
+     * @param FileFinder $finder
+     * @return ArrayCollection
+     */
+    public function findBy(FileFinder $finder);
+
+    /**
+     * @param $uuid
+     * @return File
+     */
+    public function findByUuid($uuid);
+
+    /**
+     * Finds file by filename in a folder
+     *
+     * @param Folder $folder
+     * @param $filename
+     * @return File
+     */
+    public function findByFilename(Folder $folder, $filename);
+
+    /**
+     * Finds and returns all files
+     *
+     * @return ArrayCollection
+     */
+    public function findAll();
+
+    /**
+     * Uploads a file
+     *
+     * @param  mixed            $upload Uploadable, path or object
+     * @param  Folder           $folder
+     * @return File
+     * @throws FilelibException
+     */
+    public function upload($upload, Folder $folder = null, $profile = 'default');
+
+    public function afterUpload(File $file);
+
+    /**
+     * Deletes a file
+     *
+     * @param File $file
+     */
+    public function delete(File $file);
+
+    /**
+     * Copies a file to folder
+     *
+     * @param File   $file
+     * @param Folder $folder
+     */
+    public function copy(File $file, Folder $folder);
 }
